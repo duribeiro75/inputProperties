ff61af3683818f64555bb0a897436dcc561e33a0
Eduardo Nascimento <duribeiro75@outlook.com>
2021-10-07 19:55:20 -0300

    commit input_output_properties

---
+++ src/app/rating/rating.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-rating',
+  templateUrl: './rating.component.html',
+  styleUrls: ['./rating.component.css']
+})
+export class RatingComponent {
+
+  @Input() rating!: number;
+  @Output() onRatingChange = new EventEmitter<number>();
+
+  onClick(i: number){
+    this.onRatingChange.emit(i);
+  }
+
+}
